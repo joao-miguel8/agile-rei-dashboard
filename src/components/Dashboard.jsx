3334e8498224730f55d54207f5ed6b40482734e0
[joao miguel] <83466001+joao-miguel8@users.noreply.github.com>
2023-09-11 11:49:57 -0400

    - Style/markup change for add and delete property options in Dashboard.jsx - Add classnames npm package

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -2,12 +2,13 @@ import "../App.css";
 import { db } from "../firebase-server/firebase";
 import { createPortal } from "react-dom";
 import { useEffect, useState } from "react";
-import { collection, getDocs, addDoc } from "firebase/firestore";
+import { collection, getDocs } from "firebase/firestore";
 import { setProperties } from "../redux/reducers/properties";
 import { useDispatch, useSelector } from "react-redux";
 import PropertyCard from "@/components/PropertyCard";
 import PropertyCardFormDialog from "@/components/PropertyCardFormDialog";
 import MobileHeader from "./MobileHeader";
+import classNames from "classnames";
 
 function Dashboard() {
 	const dispatch = useDispatch();
@@ -16,6 +17,7 @@ function Dashboard() {
 	console.log(properties);
 	const [deletedProperties, setDeletedProperties] = useState([]);
 	const [showCreatePropertyDialog, setShowCreatePropertyDialog] = useState(false);
+	const [isPropertiesOptionsHidden, setIsPropertiesOptionsHidden] = useState(true);
 
 	const getPropertyData = async () => {
 		try {
@@ -29,7 +31,6 @@ function Dashboard() {
 		} catch (error) {
 			console.error(error);
 		}
-		console.log("rerenderEffect01");
 	};
 
 	useEffect(() => {
@@ -64,31 +65,45 @@ function Dashboard() {
 		<div>
 			<MobileHeader />
 			<section>
-				<h3 className="mt-20 py-2 pl-4 text-22 font-medium">My Properties</h3>
-				<div className="relative px-2 flex gap-4">
-					<button onClick={handleShowPropertyDialog} className=" flex items-center h-10 text-12 font-bold gap-2 pr-2 text-neutral-50 bg-green-500">
-						<div className="flex items-center h-full text-neutral-50 bg-neutral-900 px-2 ">
-							<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-								<path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
-							</svg>
-						</div>
-						Add Property
+				{/* My Properties title and properties Options */}
+				<div className="relative mt-10 mx-4 flex justify-between">
+					<h3 className="text-22 font-medium">My Properties</h3>
+					{/* Kebab menu button */}
+					<button onClick={() => setIsPropertiesOptionsHidden(prevState => !prevState)} className="">
+						<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8 h-8">
+							<path strokeLinecap="round" strokeLinejoin="round" d="M12 6.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 12.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 18.75a.75.75 0 110-1.5.75.75 0 010 1.5z" />
+						</svg>
 					</button>
-					{showCreatePropertyDialog ? createPortal(<PropertyCardFormDialog properties={properties} setProperties={setProperties} closeDialog={handleShowPropertyDialog} />, document.getElementById("modal-portal")) : null}
-					<button onClick={() => deleteSelectedProperties()} {...(properties.length < 1 ? "disabled" : null)} className={`flex items-center h-10 text-12 font-bold gap-2 pr-2 text-neutral-50 bg-red-500 ${properties.length < 1 ? "opacity-50" : null}`}>
-						<div className="flex items-center h-full text-red-400 bg-neutral-900 px-2">
-							<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 ">
-								<path
-									strokeLinecap="round"
-									strokeLinejoin="round"
-									d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
-								/>
-							</svg>
-						</div>
-						Delete Property
-					</button>
+					<div className={classNames("w-fit overflow-hidden absolute z-40 right-[10px] top-[40px] flex flex-col text-black bg-white rounded-md shadow-[0px_3px_8px_rgb(0,0,0,0.24)]", isPropertiesOptionsHidden ? "opacity-0 duration-300" : "opacity-100 duration-300")}>
+						{/* Add button */}
+						{!isPropertiesOptionsHidden && (
+							<button onClick={handleShowPropertyDialog} className="p-4 flex items-center justify-between gap-6 hover:bg-neutral-100">
+								<span>Add Property</span>
+								<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+									<path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+								</svg>
+							</button>
+						)}
+						{/* Delete button */}
+						{!isPropertiesOptionsHidden && (
+							<button onClick={() => deleteSelectedProperties()} className={classNames("p-4 flex items-center justify-between gap-2 hover:bg-neutral-100", properties.length < 1 ? "disabled opacity-30" : null)}>
+								<span>Delete Property</span>
+								<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+									<path
+										strokeLinecap="round"
+										strokeLinejoin="round"
+										d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
+									/>
+								</svg>
+							</button>
+						)}
+						<span className="w-full h-1 bg-pink-800"></span>
+					</div>
 				</div>
-				<div className="w-full pl-2 mt-8 flex gap-4 overflow-x-auto scrollbar-hide">
+				{/* if add property button clicked render our form dialog */}
+				{showCreatePropertyDialog ? createPortal(<PropertyCardFormDialog properties={properties} setProperties={setProperties} closeDialog={handleShowPropertyDialog} />, document.getElementById("modal-portal")) : null}
+				{/* Render My Properties Listing */}
+				<div className="w-full pl-2 flex gap-4 overflow-x-auto scrollbar-hide">
 					{properties ? properties.map(property => <PropertyCard key={property.id} propertyData={property} onCardDelete={() => handleRemoveProperty(property.id)} isSelected={deletedProperties.includes(property.id)} />) : <p>Loading properties...</p>}
 				</div>
 			</section>
